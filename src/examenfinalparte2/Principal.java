62c0cb6de31656f27ab6431fe2086451f75d9225
balva <balva@DESKTOP-6PFDB9P>
2016-06-18 20:21:28 +0200

    Renombrado de variables

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -6,81 +6,87 @@ public class Principal {
 
     public static void main(String arg[]) {
         /**
-         * En esta clase main simplemente declaramos variables , y dejamos el for con algunos cambios para asi mostrar los numeros primos.
-         * Entre ellos los cambios son: Usamos el metodo ContarDigitos como contador y el calcularNumPrimo para si es true que salga el numero primo
+         * En esta clase main simplemente declaramos variables , y dejamos el
+         * for con algunos cambios para asi mostrar los numeros primos. Entre
+         * ellos los cambios son: Usamos el metodo ContarDigitos como contador y
+         * el calcularNumPrimo para si es true que salga el numero primo
          */
-        boolean p = false;
-        int dig = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un numero"));
-        int ndig = 0;
-        if (dig <= 0) {
+        boolean numPrimo = false;
+        int digito = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un numero"));
+        int numeroDigito = 0;
+        if (digito <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
-           
 
-            int contador = ContarDigitos(i) ;
-            ndig = contador;
-            p=CalcularNumPrimo(ndig,dig,i,p);
-            if(p==true){
+            int contador = ContarDigitos(i);
+            numeroDigito = contador;
+            numPrimo = CalcularNumPrimo(numeroDigito, digito, i, numPrimo);
+            if (numPrimo == true) {
                 System.out.println(i);
             }
         }
     }
-/**
- * Este metodo lo que hace es calcular numeros primos 
- * @param ndig Este parametro es el numero de digitos
- * @param dig Este es el digito
- * @param i Este simplemente guarda un numero para acabar viendo numeros primos
- * @param p Valor de boolean
- * @return 
- */
-    private static boolean CalcularNumPrimo(int ndig, int dig, int i, boolean p) {
-        if (ndig == dig) {
+
+    /**
+     * Este metodo lo que hace es calcular numeros primos
+     *
+     * @param numeroDigito Este parametro es el numero de digitos
+     * @param digito Este es el digito
+     * @param i Este simplemente guarda un numero para acabar viendo numeros
+     * primos
+     * @param numPrimo Valor de boolean
+     * @return
+     */
+    private static boolean CalcularNumPrimo(int numeroDigito, int digito, int i, boolean numPrimo) {
+        if (numeroDigito == digito) {
             if (i < 4) {
-                p = true;
+                numPrimo = true;
             } else {
                 if (i % 2 == 0) {
-                    p = false;
+                    numPrimo = false;
                 } else {
-                    int contador1 = 0;
+                    int contador = 0;
                     int i1 = 1;
-                    int k = (i - 1) / 2;
-                    if (k % 2 == 0) {
-                        k--;
+                    int limite = (i - 1) / 2;
+                    if (limite % 2 == 0) {
+                        limite--;
                     }
 
-                    while (i1 <= k) {
+                    while (i1 <= limite) {
                         if (i % i1 == 0) {
-                            contador1++;
+                            contador++;
                         }
                         i1 += 2;
-                        if (contador1 == 2) {
-                            i1 = k + 1;
+                        if (contador == 2) {
+                            i1 = limite + 1;
                         }
                     }
 
-                    if (contador1 == 1) {
-                        p = true;
+                    if (contador == 1) {
+                        numPrimo = true;
                     }
                 }
             }
 
-            if (p == true) {
+            if (numPrimo == true) {
                 System.out.println(i);
             }
         }
-        return p;
+        return numPrimo;
     }
+
     /**
-     *  Aqui lo que hacemos es un metodo que sirve para contar los digitos
-     * @param i 
+     * Aqui lo que hacemos es un metodo que sirve para contar los digitos
+     *
+     * @param i
      * @return Retorna el numero de digitos que lo sabemos gracias a un contador
      */
     private static int ContarDigitos(int i) {
-        int aux = i;
+        int auxiliar = i;
         int contador = 0;
-        while (aux != 0) {
-            aux = aux / 10;
+        while (auxiliar != 0) {
+            auxiliar= auxiliar / 10;
             contador++;
         }
         return contador;
